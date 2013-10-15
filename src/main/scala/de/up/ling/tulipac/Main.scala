76c7834b7a85b866d39687afea980f1411425b4c
Alexander Koller <koller@coli.uni-saarland.de>
2013-10-15 10:50:59 +0200

    init

---
+++ src/main/scala/de/up/ling/tulipac/Main.scala
@@ -0,0 +1,197 @@
+package de.up.ling.tulipac
+
+import scala.xml.Elem
+import java.io._
+import com.google.common.io._
+
+object Main {
+	private val treeToFamily = scala.collection.mutable.Map[String,String]()
+	private val familyToTree = scala.collection.mutable.Map[String, TreeFamily]()
+	private val treeNameToTree = scala.collection.mutable.Map[String, Tree]()
+	private val treeList = scala.collection.mutable.ListBuffer[Tree]()
+	private val wordList = scala.collection.mutable.ListBuffer[Word]()
+	
+	def main(args: Array[String]) : Unit = {
+	  val parser = new TagParser()	    
+	  val inputFile = new File(args(0))
+	  val filenameStem = Files.getNameWithoutExtension(args(0))
+	  
+	  processGrammar(inputFile, parser, "")
+	  println("Done parsing.\n")
+	  
+	  val gFilename = new File(filenameStem + "-g.xml")
+	  println(s"Writing grammar file ${gFilename} ...")
+	  val gFile = new PrintWriter(gFilename)
+	  xml.XML.write(gFile, mkGrammar(treeList), "UTF-8", xmlDecl = true, doctype = xml.dtd.DocType("mcgrammar", xml.dtd.SystemID("xmg-mctag.dtd.xml"), Nil))
+	  gFile.close()
+	  
+	  val mFilename = new File(filenameStem + "-m.xml")
+	  println(s"Writing morphology file ${mFilename} ...")
+	  val mFile = new PrintWriter(mFilename)
+	  xml.XML.write(mFile, mkMorph(wordList), "UTF-8", xmlDecl = true, doctype = xml.dtd.DocType("mcgrammar", xml.dtd.SystemID("xmg-mctag.dtd.xml"), Nil))
+	  mFile.close()
+	  
+	  val lFilename = new File(filenameStem + "-l.xml")
+	  println(s"Writing lexicon file ${lFilename} ...")
+	  val lFile = new PrintWriter(lFilename)
+	  xml.XML.write(lFile, mkLex(wordList), "UTF-8", xmlDecl = true, doctype = xml.dtd.DocType("mcgrammar", xml.dtd.SystemID("xmg-mctag.dtd.xml"), Nil))
+	  lFile.close()
+	  
+	  println("Done.")
+	}
+	
+	
+	def processGrammar(file:File, parser:TagParser, printPrefix:String) : Unit = {
+	  val reader = new FileReader(file)
+	  
+	  println(printPrefix + s"Parsing ${file} ...")
+	  val grammar: Seq[TopLevel] = try {
+	    parser.parseGrammar(reader)
+	  } catch {
+	    case e: Exception => println(e.getMessage()); System.exit(1); null
+	  }  
+	  
+	  for( topLevel <- grammar ) {
+	    topLevel match {
+	      case tf : TreeFamily => for( treeName <- tf.trees ) { treeToFamily(treeName) = tf.name; familyToTree(tf.name) = tf } 
+	      case tree : Tree => treeList += tree; treeNameToTree(tree.name) = tree
+	      case words : Words => wordList ++= words.words
+	      case Include(includedFile) => processGrammar(new File(includedFile), parser, "  " + printPrefix)
+	    }
+	  }
+	}
+	
+	
+	
+	
+	
+	def mkGrammar(inputs:Seq[Tree]) = {
+	  <mcgrammar>
+		{for (input <- inputs) yield { toXml(input) }}
+	  </mcgrammar>
+	}
+	
+	def mkMorph(words:Seq[Word]) = {
+	  <mcgrammar>
+        <morphs>
+		  { for(word <- words) yield {
+		      val cat = anchorCat(word.trees).getOrElse("***")
+		      
+		      if( cat == "***" ) {
+		        println("Warning: cannot determine anchor for word " + word.word)
+		      }
+		      
+		      <morph lex={word.word}>
+		        <lemmaref cat={cat} name={word.lemma}>
+		          {toXml(word.fs)}
+		        </lemmaref>
+		      </morph>
+		    }
+		  }
+		</morphs>
+	  </mcgrammar>
+	}
+	
+	// TODO:
+	// - group duplicate lemma entries
+	
+	def mkLex(words:Seq[Word]) = {
+      <mcgrammar>
+        <lemmas>
+		  { for( word <- words ) yield {
+		      val cat = anchorCat(word.trees).getOrElse("***")
+		      
+		      if( cat == "***" ) {
+		        println("Warning: cannot determine anchor for word " + word.word)
+		      }
+		      
+		      <lemma name={word.lemma} cat={cat}>
+		        <anchor tree_id={formatTreeName(word.trees)} />
+		      </lemma>
+		    }
+		  }
+		</lemmas>
+	  </mcgrammar>
+	}
+	
+	def formatTreeName(treeId:StringValued) = treeId match {
+	  case tree : Identifier => tree.str
+	  case family : FamilyIdentifier => s"family[@name=${family.str}]"
+	}
+	
+	def toXml(tree:Tree) : Elem = {
+	  <entry type="anc" name={tree.name}>
+	    <family>{getFamily(tree) match { case Some(tf) => tf   case None => tree.name }}</family>
+	  	<trace />
+	  	
+	  	<tree id={tree.name}>
+	  	  {toXml(tree.tree)}
+	  	</tree>
+
+	  	<semantics></semantics>
+	  	<interface></interface>
+	  </entry>
+	}
+	
+	
+  def toXml(label:String, fs:Map[String,StringValued]) : Elem = {
+    <f name={label}>
+      {toXml(fs)}
+    </f>
+  }
+  
+  def toXml(fs:Map[String,StringValued]) : Elem = {
+    <fs>
+    	{for( key <- fs.keys ) yield {
+    	    fs(key) match {
+    	      case value:Identifier => <f name={key}> <sym value={value.str} /> </f>
+    	      case value:Variable =>   <f name={key}> <sym varname={value.strWithAt} /> </f>
+    	    }
+    	  }
+    	}
+      </fs>
+  }
+  
+  def toXml(node:Node) : Elem = {
+    <node type={node.nodeType}>
+      <narg>
+    	<fs>
+    		<f name="cat"> <sym value={node.cat} /> </f>
+    		{toXml("top", node.top)}
+    		{toXml("bot", node.bottom)}
+    	</fs>
+      </narg>
+
+      {for( child <- node.children ) yield toXml(child)}
+    </node>
+  }
+  
+  
+  
+  
+  private def anchorCat(tree:Node) : Option[String] = {
+    if( tree.nodeType == "anchor" ) {
+      Some(tree.cat)
+    } else {
+      val ret = tree.children.map { anchorCat(_) }.flatten
+      
+      if( ret.isEmpty ) {
+        None
+      } else {
+        Some(ret(0))
+      }
+    }
+  }
+  
+  private def anchorCat(family:TreeFamily) : Option[String] = treeNameToTree.get (family.trees(0)).flatMap { tree:Tree => anchorCat(tree.tree) }
+  
+  private def anchorCat(name: StringValued) : Option[String] = {
+    name match {
+      case Identifier(treename) => treeNameToTree.get(treename).flatMap { tree:Tree => anchorCat(tree.tree) }
+      case FamilyIdentifier(familyname) => familyToTree.get(familyname).flatMap { family : TreeFamily => anchorCat(family) }
+    }
+  }
+  
+  private def getFamily(tree:Tree) = treeToFamily.get(tree.name)
+}
+
---
+++ src/main/scala/de/up/ling/tulipac/TagParser.scala
@@ -0,0 +1,131 @@
+package de.up.ling.tulipac
+
+import scala.util.parsing.combinator._
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.RegexParser
+import java.io.Reader
+
+class TagParser extends RegexParsers {
+	def parseGrammar(reader: Reader) : Seq[TopLevel] = {
+	  parseAll(phrase(grammar), reader) match {
+	    case Success(g, _) => g
+	    case x: Failure => throw new RuntimeException(x.toString())
+	    case x: Error => throw new RuntimeException(x.toString())
+	  }
+	}
+  
+  
+    // ignore ordinary whitespace, line comments, and inline comments between combinators
+    // adapted from http://pastebin.com/2GvtvSPb
+    override val whiteSpace = "(?sm)(\\s*(?://.*?$|/\\*((?!\\*/).)*\\*/)\\s*|\\s+)+".r
+  
+    // \p{L} should match any (Unicode) letter, but somehow it still doesn't work
+	def rawIdentifier = """[\p{L}_][\p{L}_0-9]*""".r ^^ { s => new Identifier(s) }
+	def quotedIdentifier = """\'[^']*\'""".r ^^ { s => new Identifier(s.substring(1, s.length()-1))}
+	def doubleQuotedIdentifier = """\"[^"]*\"""".r ^^ { s => new Identifier(s.substring(1, s.length()-1))}
+	def familyIdentifier = """<[^>]*>""".r ^^ { s => new FamilyIdentifier(s.substring(1, s.length()-1))}
+	def annotation = """@[\p{L}_0-9]+""".r ^^ { s => new Annotation(s.substring(1)) }
+	def variable = """\?[\p{L}_0-9]+""".r ^^ { s => new Variable(s.substring(1)) }
+	
+	def substitutionMarker = """!""".r ^^ { _ => new SubstitutionMarker() }
+	def footMarker = """\*""".r ^^ { _ => new FootMarker() }
+	def anchorMarker = """\+""".r ^^ { _ => new AnchorMarker() }
+	
+	def identifier : Parser[Identifier] = rawIdentifier | quotedIdentifier | doubleQuotedIdentifier ^^ { m => m }
+	def marker : Parser[Marker] = substitutionMarker | footMarker | anchorMarker ^^ { m => m }
+
+	def grammar : Parser[Seq[TopLevel]] = rep(tree | family | wordByItself | lemma | include) ^^ { g => g }
+	
+	/**** trees ****/
+	def tree : Parser[Tree] = "tree" ~ identifier ~ ":" ~ node ^^ {
+	  case _ ~ name ~ _ ~ tree => new Tree(name.str, tree)
+	}
+	
+	private def get(fs:Option[Map[String,StringValued]]) = fs.getOrElse(Map())
+	
+	def node : Parser[Node] = identifier ~ rep(annotation | marker) ~ opt(fs) ~ opt(fs) ~ opt("{" ~ rep(node) ~ "}") ^^ {
+	  case cat ~ markers ~ topOpt ~ botOpt ~ childrenOpt =>
+	    val top = get(topOpt)
+	    val bot = get(botOpt)	    
+	    val children : Seq[Node] = childrenOpt match {
+	      case Some(_ ~ co ~ _) => co
+	      case None => Nil
+	    }
+	    
+	    new Node(cat.str, markers, top, bot, children)
+	}
+	
+	def ft : Parser[(String,StringValued)] = identifier ~ "=" ~ (identifier | variable) ^^ {
+	  case key ~ _ ~ (value:StringValued) => (key.str -> value)
+	}
+	
+	def fs : Parser[Map[String,StringValued]] = "[" ~ repsep(ft,",") ~ "]" ^^ {
+	  case _ ~ fts ~ _ => Map() ++ fts
+	}
+	
+	/**** tree families ****/
+	def family : Parser[TreeFamily] = "family" ~ identifier ~ ":" ~ "{" ~ repsep(identifier, ",") ~ "}" ^^ {
+	  case _ ~ name ~ _ ~ _ ~ trees ~ _ => new TreeFamily(name.str, trees.map { t => t.str })
+	}
+	
+	
+	/**** words ****/
+	def wordByItself : Parser[Words] = "word" ~ identifier ~ ":" ~ (identifier|familyIdentifier) ~ opt(fs) ^^ {
+	  case _ ~ word ~ _ ~ treeName ~ fs => new Words(Array(new Word(word.str, word.str, treeName, get(fs))))
+	}
+	
+	def wordInLemma : Parser[Word] = "word" ~ identifier ~ opt(":" ~ fs) ^^ {
+	  case _ ~ word ~ rest => rest match {
+	    case Some(_ ~ fs) => new Word(word.str, null, null, fs)
+	    case None         => new Word(word.str, null, null, Map())     
+	  } 
+	}
+	
+	def lemma : Parser[Words] = "lemma" ~ identifier ~ ":" ~ (identifier|familyIdentifier) ~ opt(fs) ~ "{" ~ rep(wordInLemma) ~ "}" ^^ {
+	  case _ ~ lemma ~ _ ~ treeName ~ fs ~ _ ~ words ~ _ => new Words(words.map { word:Word => new Word(word.word, lemma.str, treeName, get(fs) ++ word.fs) })
+	}
+	
+	/**** #include ****/
+	def include : Parser[Include] = "#include" ~ identifier ^^ {
+	  case _ ~ filename => new Include(filename.str)
+	}
+}
+
+
+sealed abstract class Expression
+
+class StringValued(val str:String) extends Expression 
+case class Identifier(string:String) extends StringValued(string)
+case class FamilyIdentifier(string:String) extends StringValued(string)
+case class Annotation(string:String) extends StringValued(string)
+case class Variable(string:String) extends StringValued(string) {
+  def strWithAt = "@" + string 
+}
+
+class Marker extends Expression
+case class SubstitutionMarker extends Marker
+case class FootMarker extends Marker
+case class AnchorMarker extends Marker
+
+case class Node(cat:String, markers:Seq[Expression], top:Map[String,StringValued], bottom:Map[String,StringValued], children:Seq[Node]) extends Expression {
+  def nodeType = {
+    if( markers.contains(new SubstitutionMarker()) ) {
+      "subst"
+    } else if( markers.contains(new AnchorMarker())) {
+      "anchor"
+    } else if( markers.contains(new FootMarker())) {
+      "foot"
+    } else if( markers.contains(new Annotation("NA"))) {
+      "nadj"
+    } else {
+      "std"
+    }
+  }  
+}
+
+class TopLevel extends Expression
+case class Tree(name:String, tree:Node) extends TopLevel
+case class TreeFamily(name:String, trees:Seq[String]) extends TopLevel
+case class Word(word:String, lemma:String, trees:StringValued, fs:Map[String,StringValued])
+case class Words(words:Seq[Word]) extends TopLevel
+case class Include(filename:String) extends TopLevel
+
